d5904af7fb33ce07a0794ad3070d4f4174206f4d
ByiFire <servalnol@gmail.com>
2025-11-19 10:32:37 +0100

    projet principal done

--- Projet_PA.cpp
+++ Projet_PA.cpp
@@ -1,5 +1,6 @@
 #include <windows.h>
 #include <gdiplus.h>
+#include <vector>
 #include <string>
 
 #pragma comment(lib, "gdiplus.lib")
@@ -7,207 +8,504 @@
 using namespace Gdiplus;
 using namespace std;
 
-Gdiplus::Image* g_pImage = nullptr;
-HWND g_hEditMessage = nullptr;
-HWND g_hBtnValidate = nullptr;
-wstring g_userMessage;
+const DWORD MAGIC = 0x5354454D;
+const int HEADER_SIZE = 8;
 
-// IDs
+Gdiplus::Image* g_img = nullptr;
+vector<BYTE> g_data;
+int g_w = 0, g_h = 0;
+
+ULONG_PTR gdiplusToken;
+
+const wchar_t* ENCODE_CLASS = L"EncodeDialog";
+const wchar_t* DECODE_CLASS = L"DecodeDialog";
+
 enum {
-    LOAD_IMAGE = 2001,   // Nouveau : charger depuis fichier
+    LOAD_IMAGE = 2001,
     WRITE_MESSAGE = 102,
-    SHOW_MENU = 103,
     DECODE_MESSAGE = 104,
     SAVE_IMAGE = 105,
-
-    BUTTON_LOAD_IMG = 1000,
-    BTN_VALIDATE = 1001,
-    EDIT_MESSAGE = 1002
+    BTN_OK = 1001,
+    BTN_CANCEL = 1002,
+    EDIT_MESSAGE = 1003
 };
 
-LRESULT CALLBACK MainWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK WindowProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK EncodeDialog(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK DecodeDialog(HWND, UINT, WPARAM, LPARAM);
+void ShowModalDialog(HWND parent, const wchar_t* className, const wchar_t* title, int width, int height);
 
-HMENU hMenuMain;
-HMENU hMenuParam;
+void SetLSB(BYTE& p, int b) { p = (p & 0xFE) | b; }
+int GetLSB(BYTE p) { return p & 1; }
 
-ULONG_PTR gdiplusToken; // nécessaire pour GDI+
-
-// WinMain
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
+bool LoadImageFromFile(const wchar_t* path)
 {
+    if (g_img) delete g_img;
+    g_img = new Gdiplus::Image(path);
 
-    // Initialisation GDI+
-    GdiplusStartupInput gdiplusStartupInput;
-    GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
-
-    // Classe de fenêtre
-    WNDCLASS wc = {};
-    wc.lpfnWndProc = MainWindowProc;
-    wc.hInstance = hInstance;
-    wc.lpszClassName = L"MainWindow";
-    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-
-    if (!RegisterClass(&wc)) return 0;
-
-    // Fenêtre principale
-    HWND hwnd = CreateWindowEx(
-        0, L"MainWindow", L"Projet Prog. Avancee", WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, 900, 600,
-        NULL, NULL, hInstance, NULL
-    );
-
-    if (!hwnd) return 0;
-
-    // Menus
-    hMenuMain = CreateMenu();
-    hMenuParam = CreateMenu();
-
-    AppendMenu(hMenuParam, MF_STRING, LOAD_IMAGE, L"Afficher une image...");
-    AppendMenu(hMenuParam, MF_STRING, WRITE_MESSAGE, L"Ecrire un Message");
-    AppendMenu(hMenuParam, MF_STRING, DECODE_MESSAGE, L"Decoder le texte");
-    AppendMenu(hMenuParam, MF_STRING, SAVE_IMAGE, L"Sauvegarder l'image");
-    AppendMenu(hMenuMain, MF_POPUP, (UINT_PTR)hMenuParam, L"Parametres");
-
-    SetMenu(hwnd, hMenuMain);
-
-    // Afficher
-    ShowWindow(hwnd, nCmdShow);
-    UpdateWindow(hwnd);
-
-    // boucle de messages
-    MSG msg;
-    while (GetMessage(&msg, NULL, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
+    if (g_img->GetLastStatus() != Ok) {
+        delete g_img;
+        g_img = nullptr;
+        return false;
     }
 
-    // arrêter GDI+
-    GdiplusShutdown(gdiplusToken);
+    g_w = g_img->GetWidth();
+    g_h = g_img->GetHeight();
 
-    return (int)msg.wParam;
+    Bitmap* bmp = new Bitmap(g_w, g_h, PixelFormat32bppARGB);
+    Graphics* gfx = Graphics::FromImage(bmp);
+    gfx->DrawImage(g_img, 0, 0, g_w, g_h);
+    delete gfx;
+
+    BitmapData bd;
+    Rect rect(0, 0, g_w, g_h);
+    bmp->LockBits(&rect, ImageLockModeRead, PixelFormat32bppARGB, &bd);
+
+    g_data.resize(g_w * g_h * 4);
+    memcpy(g_data.data(), bd.Scan0, g_data.size());
+
+    bmp->UnlockBits(&bd);
+    delete bmp;
+    return true;
 }
 
-// WndProc
-LRESULT CALLBACK MainWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+bool EmbedMessage(const wstring& msg)
 {
-    switch (uMsg)
+    if (g_data.empty()) return false;
+
+    // Convertir en UTF-8
+    int utf8Sz = WideCharToMultiByte(CP_UTF8, 0, msg.c_str(), -1, 0, 0, 0, 0);
+    if (utf8Sz <= 1) return false;
+    vector<char> utf8(utf8Sz);
+    WideCharToMultiByte(CP_UTF8, 0, msg.c_str(), -1, utf8.data(), utf8Sz, 0, 0);
+    DWORD len = utf8Sz - 1; // sans le NUL
+
+    // Capacité en bits : 3 canaux par pixel (B,G,R), g_data a 4 octets/pixel
+    long pixelCount = g_w * g_h;
+    long capacityBits = pixelCount * 3; // 3 bits par pixel (un bit par canal)
+    long neededBits = (HEADER_SIZE + (long)len) * 8L;
+
+    if (neededBits > capacityBits) {
+        MessageBox(0, L"Message trop long pour cette image !", L"Erreur", MB_OK);
+        return false;
+    }
+
+    // Ecrire bit à bit : mapping k-th bit -> pixel = k / 3, channel = k % 3 (0=B,1=G,2=R)
+    auto setDataBit = [&](long k, int bit) {
+        long pixelIdx = k / 3;
+        int channel = k % 3; // 0..2
+        long byteIndex = pixelIdx * 4 + channel; // skip alpha (channel 3)
+        SetLSB(g_data[byteIndex], bit);
+        };
+
+    long k = 0;
+    // MAGIC (32 bits)
+    for (int i = 0; i < 32; ++i) {
+        int bit = (int)((MAGIC >> (31 - i)) & 1);
+        setDataBit(k++, bit);
+    }
+    // LENGTH (32 bits)
+    for (int i = 0; i < 32; ++i) {
+        int bit = (int)((len >> (31 - i)) & 1);
+        setDataBit(k++, bit);
+    }
+    // DATA
+    for (DWORD i = 0; i < len; ++i) {
+        unsigned char c = (unsigned char)utf8[i];
+        for (int b = 0; b < 8; ++b) {
+            int bit = (c >> (7 - b)) & 1;
+            setDataBit(k++, bit);
+        }
+    }
+
+    return true;
+}
+
+wstring ExtractMessage()
+{
+    if (g_data.empty()) return L"";
+
+    long pixelCount = g_w * g_h;
+    long capacityBits = pixelCount * 3;
+
+    auto getDataBit = [&](long k) -> int {
+        long pixelIdx = k / 3;
+        int channel = k % 3;
+        long byteIndex = pixelIdx * 4 + channel;
+        return GetLSB(g_data[byteIndex]);
+        };
+
+    long k = 0;
+    // lire MAGIC
+    DWORD magic = 0;
+    for (int i = 0; i < 32; ++i) {
+        if (k >= capacityBits) return L"[Aucun message detecte]";
+        magic = (magic << 1) | (DWORD)getDataBit(k++);
+    }
+    if (magic != MAGIC) return L"[Aucun message detecte]";
+
+    // lire LENGTH
+    DWORD len = 0;
+    for (int i = 0; i < 32; ++i) {
+        if (k >= capacityBits) return L"[Erreur: longueur invalide]";
+        len = (len << 1) | (DWORD)getDataBit(k++);
+    }
+    // sanity
+    if (len == 0 || (long)len * 8L + k > capacityBits) return L"[Erreur: longueur invalide]";
+
+    vector<char> utf8(len + 1);
+    for (DWORD i = 0; i < len; ++i) {
+        unsigned char c = 0;
+        for (int b = 0; b < 8; ++b) {
+            if (k >= capacityBits) return L"[Erreur lors de la lecture]";
+            c = (unsigned char)((c << 1) | (unsigned char)getDataBit(k++));
+        }
+        utf8[i] = (char)c;
+    }
+    utf8[len] = 0;
+
+    int wSz = MultiByteToWideChar(CP_UTF8, 0, utf8.data(), -1, 0, 0);
+    if (wSz <= 1) return L"";
+
+    wstring res(wSz - 1, 0);
+    MultiByteToWideChar(CP_UTF8, 0, utf8.data(), -1, &res[0], wSz);
+    return res;
+}
+
+bool SaveBMP(const wchar_t* path)
+{
+    if (g_data.empty()) return false;
+
+    int rowSz = ((g_w * 3 + 3) / 4) * 4;
+    int imgSz = rowSz * g_h;
+
+    BITMAPFILEHEADER fh = {
+        0x4D42,
+        (DWORD)(sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER) + imgSz),
+        0, 0,
+        sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER)
+    };
+
+    BITMAPINFOHEADER ih = {
+        sizeof(BITMAPINFOHEADER),
+        g_w, g_h, 1,
+        24, BI_RGB,
+        (DWORD)imgSz
+    };
+
+    HANDLE hf = CreateFile(
+        path, GENERIC_WRITE, 0, 0,
+        CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0
+    );
+
+    if (hf == INVALID_HANDLE_VALUE) return false;
+
+    DWORD wr;
+    WriteFile(hf, &fh, sizeof(fh), &wr, 0);
+    WriteFile(hf, &ih, sizeof(ih), &wr, 0);
+
+    vector<BYTE> row(rowSz);
+
+    for (int y = g_h - 1; y >= 0; y--) {
+        for (int x = 0; x < g_w; x++) {
+            int i = (y * g_w + x) * 4;
+            row[x * 3] = g_data[i];
+            row[x * 3 + 1] = g_data[i + 1];
+            row[x * 3 + 2] = g_data[i + 2];
+        }
+        WriteFile(hf, row.data(), rowSz, &wr, 0);
+    }
+
+    CloseHandle(hf);
+    return true;
+}
+
+void ShowModalDialog(HWND parent, const wchar_t* className, const wchar_t* title, int width, int height)
+{
+    HWND dialog = CreateWindowEx(
+        WS_EX_DLGMODALFRAME | WS_EX_TOPMOST,
+        className,
+        title,
+        WS_VISIBLE | WS_CAPTION | WS_SYSMENU,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        width, height,
+        parent,
+        NULL,
+        (HINSTANCE)GetWindowLongPtr(parent, GWLP_HINSTANCE),
+        NULL
+    );
+
+    RECT rp, rd;
+    GetWindowRect(parent, &rp);
+    GetWindowRect(dialog, &rd);
+
+    SetWindowPos(
+        dialog,
+        HWND_TOP,
+        rp.left + (rp.right - rp.left - (rd.right - rd.left)) / 2,
+        rp.top + (rp.bottom - rp.top - (rd.bottom - rd.top)) / 2,
+        0, 0,
+        SWP_NOSIZE
+    );
+
+    EnableWindow(parent, FALSE);
+
+    MSG msg;
+    while (IsWindow(dialog)) {
+        if (GetMessage(&msg, NULL, 0, 0)) {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    EnableWindow(parent, TRUE);
+    SetFocus(parent);
+}
+
+LRESULT CALLBACK EncodeDialog(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+    static HWND hEdit;
+
+    switch (msg)
+    {
+    case WM_CREATE:
+        CreateWindow(L"STATIC", L"Message a cacher :", WS_VISIBLE | WS_CHILD,
+            20, 20, 360, 20, hwnd, 0, 0, 0);
+
+        hEdit = CreateWindowEx(
+            WS_EX_CLIENTEDGE, L"EDIT", L"",
+            WS_VISIBLE | WS_CHILD | WS_BORDER | ES_AUTOHSCROLL,
+            20, 50, 360, 25, hwnd, (HMENU)EDIT_MESSAGE, 0, 0
+        );
+
+        CreateWindow(L"BUTTON", L"OK", WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+            220, 90, 80, 30, hwnd, (HMENU)BTN_OK, 0, 0);
+
+        CreateWindow(L"BUTTON", L"Annuler", WS_VISIBLE | WS_CHILD,
+            310, 90, 80, 30, hwnd, (HMENU)BTN_CANCEL, 0, 0);
+        break;
+
+    case WM_COMMAND:
+        switch (LOWORD(wp)) {
+        case BTN_OK:
+        {
+            wchar_t buf[512];
+            GetWindowText(hEdit, buf, 512);
+
+            if (wcslen(buf) == 0)
+                MessageBox(hwnd, L"Entrez un message.", L"Erreur", MB_OK);
+            else if (EmbedMessage(buf)) {
+                MessageBox(hwnd, L"Message cache !", L"Succes", MB_OK);
+                DestroyWindow(hwnd);
+            }
+        }
+        break;
+
+        case BTN_CANCEL:
+            DestroyWindow(hwnd);
+            break;
+        }
+        break;
+
+    case WM_CLOSE:
+        DestroyWindow(hwnd);
+        break;
+
+    default:
+        return DefWindowProc(hwnd, msg, wp, lp);
+    }
+    return 0;
+}
+
+LRESULT CALLBACK DecodeDialog(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+    switch (msg)
+    {
+    case WM_CREATE:
+    {
+        CreateWindow(L"STATIC", L"Message trouve :", WS_VISIBLE | WS_CHILD,
+            20, 20, 360, 20, hwnd, 0, 0, 0);
+
+        HWND hEdit = CreateWindowEx(
+            WS_EX_CLIENTEDGE, L"EDIT", L"",
+            WS_VISIBLE | WS_CHILD | WS_BORDER | ES_MULTILINE | ES_READONLY,
+            20, 50, 360, 100, hwnd, 0, 0, 0);
+
+        wstring msgDecoded = ExtractMessage();
+        SetWindowText(hEdit, msgDecoded.c_str());
+
+        CreateWindow(L"BUTTON", L"OK", WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+            310, 160, 80, 30, hwnd, (HMENU)BTN_OK, 0, 0);
+    }
+    break;
+
+    case WM_COMMAND:
+        if (LOWORD(wp) == BTN_OK)
+            DestroyWindow(hwnd);
+        break;
+
+    case WM_CLOSE:
+        DestroyWindow(hwnd);
+        break;
+
+    default:
+        return DefWindowProc(hwnd, msg, wp, lp);
+    }
+    return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+    switch (msg)
     {
     case WM_COMMAND:
-    {
-        int id = LOWORD(wParam);
-        switch (id)
+        switch (LOWORD(wp)) {
+
+        case LOAD_IMAGE:
         {
+            OPENFILENAME ofn = { sizeof(ofn) };
+            wchar_t path[MAX_PATH] = L"";
 
-        case BUTTON_LOAD_IMG:
-            // Charge test.bmp en mémoire
-            if (g_pImage) { delete g_pImage; g_pImage = nullptr; }
-            g_pImage = new Image(L"test.bmp");
-            MessageBox(hwnd, L"Image chargee ! Cliquez sur 'Afficher l'image'.", L"Info", MB_OK);
-            break;
-
-
-        case LOAD_IMAGE: // menu → choisir fichier
-        {
-            OPENFILENAME ofn;
-            wchar_t filePath[MAX_PATH] = L"";
-
-            ZeroMemory(&ofn, sizeof(ofn));
-            ofn.lStructSize = sizeof(ofn);
             ofn.hwndOwner = hwnd;
-            ofn.lpstrFilter = L"Images (*.bmp)\0*.bmp;\0";
-            ofn.lpstrFile = filePath;
+            ofn.lpstrFilter = L"Images\0*.bmp;*.png;*.jpg\0";
+            ofn.lpstrFile = path;
             ofn.nMaxFile = MAX_PATH;
-            ofn.Flags = OFN_FILEMUSTEXIST | OFN_PATHMUSTEXIST;
+            ofn.Flags = OFN_FILEMUSTEXIST;
 
             if (GetOpenFileName(&ofn)) {
-                if (g_pImage) { delete g_pImage; g_pImage = nullptr; }
-
-                g_pImage = new Gdiplus::Image(filePath);
-                InvalidateRect(hwnd, NULL, TRUE);
+                if (LoadImageFromFile(path)) {
+                    InvalidateRect(hwnd, NULL, TRUE);
+                    MessageBox(hwnd, L"Image chargee!", L"OK", MB_OK);
+                }
+                else MessageBox(hwnd, L"Erreur chargement.", L"Erreur", MB_OK);
             }
             break;
         }
 
         case WRITE_MESSAGE:
-        {
-            if (!g_hEditMessage)
-            {
-                g_hEditMessage = CreateWindowEx(
-                    0, L"EDIT", L"",
-                    WS_VISIBLE | WS_CHILD | WS_BORDER | ES_LEFT,
-                    50, 100, 300, 25,
-                    hwnd, (HMENU)EDIT_MESSAGE,
-                    (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE),
-                    NULL
-                );
-
-                g_hBtnValidate = CreateWindowEx(
-                    0, L"BUTTON", L"Valider",
-                    WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
-                    360, 100, 80, 25,
-                    hwnd, (HMENU)BTN_VALIDATE,
-                    (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE),
-                    NULL
-                );
+            if (g_data.empty()) {
+                MessageBox(hwnd, L"Chargez une image d'abord!", L"Erreur", MB_OK);
+                break;
             }
-            break;
-        }
 
-        case BTN_VALIDATE:
-        {
-            wchar_t buffer[512];
-            GetWindowText(g_hEditMessage, buffer, 512);
-            g_userMessage = buffer;
-
-            MessageBox(hwnd, g_userMessage.c_str(), L"Message reçu", MB_OK);
-
-            DestroyWindow(g_hEditMessage);
-            DestroyWindow(g_hBtnValidate);
-            g_hEditMessage = nullptr;
-            g_hBtnValidate = nullptr;
-            break;
-        }
-
-        case SHOW_MENU:
+            ShowModalDialog(hwnd, ENCODE_CLASS, L"Cacher un message", 420, 180);
             InvalidateRect(hwnd, NULL, TRUE);
             break;
 
+        case DECODE_MESSAGE:
+            if (g_data.empty()) {
+                MessageBox(hwnd, L"Chargez une image d'abord!", L"Erreur", MB_OK);
+                break;
+            }
+
+            ShowModalDialog(hwnd, DECODE_CLASS, L"Decoder un message", 420, 230);
+            break;
+
+        case SAVE_IMAGE:
+        {
+            if (g_data.empty()) {
+                MessageBox(hwnd, L"Aucune image !", L"Erreur", MB_OK);
+                break;
+            }
+
+            OPENFILENAME ofn = { sizeof(ofn) };
+            wchar_t path[MAX_PATH] = L"image_cachee.bmp";
+
+            ofn.hwndOwner = hwnd;
+            ofn.lpstrFilter = L"BMP\0*.bmp\0";
+            ofn.lpstrFile = path;
+            ofn.nMaxFile = MAX_PATH;
+            ofn.lpstrDefExt = L"bmp";
+            ofn.Flags = OFN_OVERWRITEPROMPT;
+
+            if (GetSaveFileName(&ofn)) {
+                if (SaveBMP(path))
+                    MessageBox(hwnd, L"Image sauvegardee !", L"OK", MB_OK);
+                else
+                    MessageBox(hwnd, L"Erreur sauvegarde.", L"Erreur", MB_OK);
+            }
+            break;
         }
-    }
-    break;
+        }
+        break;
 
     case WM_PAINT:
     {
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hwnd, &ps);
 
-        Graphics gfx(hdc);
-
-        if (g_pImage) {
+        if (g_img) {
+            Graphics gfx(hdc);
             RECT rc;
-            GetClientRect(hwnd, &rc);  // récupère la taille de la fenêtre
-            int width = rc.right - rc.left;
-            int height = rc.bottom - rc.top;
-
-
-            gfx.DrawImage(g_pImage, 0, 0, width, height);
+            GetClientRect(hwnd, &rc);
+            gfx.DrawImage(g_img, 0, 0, rc.right, rc.bottom);
         }
 
         EndPaint(hwnd, &ps);
+        break;
     }
-    break;
 
     case WM_CLOSE:
         DestroyWindow(hwnd);
         break;
 
     case WM_DESTROY:
+        if (g_img) delete g_img;
         PostQuitMessage(0);
         break;
 
     default:
-        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+        return DefWindowProc(hwnd, msg, wp, lp);
+    }
+    return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int nShow)
+{
+    GdiplusStartupInput gsi;
+    GdiplusStartup(&gdiplusToken, &gsi, NULL);
+
+    WNDCLASS wc = { 0, WindowProc, 0, 0, hInst, 0,
+                    0, (HBRUSH)(COLOR_WINDOW + 1), 0, L"MainWindow" };
+    RegisterClass(&wc);
+
+    WNDCLASS wc2 = { 0, EncodeDialog, 0, 0, hInst, 0,
+                     LoadCursor(0, IDC_ARROW), (HBRUSH)(COLOR_WINDOW + 1),
+                     0, ENCODE_CLASS };
+    RegisterClass(&wc2);
+
+    WNDCLASS wc3 = { 0, DecodeDialog, 0, 0, hInst, 0,
+                     LoadCursor(0, IDC_ARROW), (HBRUSH)(COLOR_WINDOW + 1),
+                     0, DECODE_CLASS };
+    RegisterClass(&wc3);
+
+    HWND hwnd = CreateWindowEx(
+        0, L"MainWindow", L"Steganographie LSB",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        900, 600,
+        0, 0, hInst, 0
+    );
+
+    HMENU hMenu = CreateMenu();
+    HMENU hSub = CreateMenu();
+
+    AppendMenu(hSub, MF_STRING, LOAD_IMAGE, L"Charger image");
+    AppendMenu(hSub, MF_STRING, WRITE_MESSAGE, L"Cacher message");
+    AppendMenu(hSub, MF_STRING, DECODE_MESSAGE, L"Decoder message");
+    AppendMenu(hSub, MF_STRING, SAVE_IMAGE, L"Sauvegarder image");
+
+    AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hSub, L"Options");
+    SetMenu(hwnd, hMenu);
+
+    ShowWindow(hwnd, nShow);
+
+    MSG msg;
+    while (GetMessage(&msg, 0, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
     }
 
-    return 0;
+    GdiplusShutdown(gdiplusToken);
+    return msg.wParam;
 }
